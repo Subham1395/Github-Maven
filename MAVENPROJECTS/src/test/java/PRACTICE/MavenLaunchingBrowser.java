d6b6de9716260d90187caf2c4006004d7d34c053
Subham1395 <126956574+Subham1395@users.noreply.github.com>
2023-08-30 08:20:08 +0530

    Update MavenLaunchingBrowser.java

--- MAVENPROJECTS/src/test/java/PRACTICE/MavenLaunchingBrowser.java
+++ MAVENPROJECTS/src/test/java/PRACTICE/MavenLaunchingBrowser.java
@@ -8,30 +8,30 @@ public class MavenLaunchingBrowser {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		FileInputStream fids = new FileInputStream("src/test/resources/commondata.properties.txt");
-		Properties poj= new Properties();
-		poj.load(fids);
-		String BROSWER = poj.getProperty("browser");
-		WebDriver driver;
-		if(BROWSER.equalsIgnoreCase("chrome"))
-		{
-			WebDriverManager.chromedriver().setup();
-			 driver = new ChromeDriver();
-		}
-		else if(BROWSER.equalsIgnoreCase("firefox"))
-		{
-			WebDriverManager.firefoxdriver().setup();
-			 driver = new FirefoxDriver();
-		}
-		else if (BROWSER.equalsIgnoreCase("edge"))
-		{
-			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
-		}
-		else
-		{
-			driver = new ChromeDriver();
-		}
+		// FileInputStream fids = new FileInputStream("src/test/resources/commondata.properties.txt");
+		// Properties poj= new Properties();
+		// poj.load(fids);
+		// String BROSWER = poj.getProperty("browser");
+		// WebDriver driver;
+		// if(BROWSER.equalsIgnoreCase("chrome"))
+		// {
+		// 	WebDriverManager.chromedriver().setup();
+		// 	 driver = new ChromeDriver();
+		// }
+		// else if(BROWSER.equalsIgnoreCase("firefox"))
+		// {
+		// 	WebDriverManager.firefoxdriver().setup();
+		// 	 driver = new FirefoxDriver();
+		// }
+		// else if (BROWSER.equalsIgnoreCase("edge"))
+		// {
+		// 	WebDriverManager.edgedriver().setup();
+		// 	driver = new EdgeDriver();
+		// }
+		// else
+		// {
+		// 	driver = new ChromeDriver();
+		// }
 		
 
 	
